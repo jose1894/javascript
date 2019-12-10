f440401d64880486e970131d4e0a24bb816b9394
Jose Guerra <jose1894@gmail.com>
2019-12-10 17:26:26 -0500

    data-types

---
+++ javascript-fundamentals/data-types/data-types.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
+* What is the output of the script?
+
+ */
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+//"name" It's a string
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
